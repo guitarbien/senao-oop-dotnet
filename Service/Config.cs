3ac25fb14021de2505d34d8c3c5a8e9a97f72ba0
guitarbien <guitarbien@gmail.com>
2017-10-27 13:00:42 +0800

    property 寫在前面，調整註解

--- Service/Config.cs
+++ Service/Config.cs
@@ -5,17 +5,35 @@ namespace Service
     public class Config
     {
         /// <summary>
-        /// assign fields
+        /// properties
         /// </summary>
-        /// <param name="ext">檔案格式</param>
-        /// <param name="location">要備份檔案的目錄</param>
-        /// <param name="subDirectory">是否處理子目錄，`true` : 處理子目錄；`false` :  `不` 處理子目錄</param>
-        /// <param name="unit">備份單位，`file` : 以單一檔案為處理單位；`directory` : 以整個目錄為處理單位</param>
-        /// <param name="remove">處理完是否刪除檔案，`true` : 刪除；`false` : 不刪除</param>
-        /// <param name="handler">`zip`  : 壓縮；`encode` : 加密</param>
-        /// <param name="destination">處理後要儲存到什麼地方，`directory` : 目錄；`db` : 資料庫</param>
-        /// <param name="dir">處理後的目錄</param>
-        /// <param name="connectionString">資料庫連接字串</param>
+        /// 檔案格式
+        public string Ext { get; }
+
+        /// 要備份檔案的目錄
+        public string Location { get; }
+
+        /// 是否處理子目錄 true:處理子目錄 / false:不處理子目錄
+        public bool SubDirectory { get; }
+
+        /// 備份單位 file:以單一檔案為處理單位 / directory:以整個目錄為處理單位
+        public string Unit { get; }
+
+        /// 處理完是否刪除檔案 true:刪除 / false:不刪除
+        public bool Remove { get; }
+
+        /// 處理方式 zip:壓縮 / encode:加密
+        public string Handler { get; }
+
+        /// 處理後要儲存到什麼地方 directory:目錄 / db資料庫
+        public string Destination { get; }
+
+        /// 處理後的目錄
+        public string Dir { get; }
+
+        /// 資料庫連接字串
+        public string ConnectionString { get; }
+
         public Config(string ext, string location, bool subDirectory, string unit, bool remove, string handler,
             string destination, string dir, string connectionString)
         {
@@ -29,18 +47,5 @@ namespace Service
             Dir = dir ?? throw new ArgumentNullException(nameof(dir));
             ConnectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
         }
-
-        /// <summary>
-        /// properties
-        /// </summary>
-        public string Ext { get; }
-        public string Location { get; }
-        public bool SubDirectory { get; }
-        public string Unit { get; }
-        public bool Remove { get; }
-        public string Handler { get; }
-        public string Destination { get; }
-        public string Dir { get; }
-        public string ConnectionString { get; }
     }
 }
