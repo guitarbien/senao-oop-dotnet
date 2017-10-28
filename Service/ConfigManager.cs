a6e68e2e8da9514fd480eab811041dfadf538a92
guitarbien <guitarbien@gmail.com>
2017-10-28 15:06:33 +0800

    ConfigManager有Count屬性可以使用操作array方式取得configs的內容

---
+++ Service/ConfigManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Service
+{
+    public class ConfigManager
+    {
+        private readonly List<Config> _configs = new List<Config>();
+
+        // access object peroperty with index style
+        public Config this[int index] => _configs[index];
+
+        public int Count => _configs.Count;
+
+        public void ProcessConfig()
+        {
+            _configs.Add(new Config(
+                "ext",
+                "location",
+                true,
+                "unit",
+                false,
+                "handler",
+                "destination",
+                "dir",
+                "connectionString"
+            ));
+        }
+    }
+}
---
+++ ServiceTest/ConfigManagerTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Service;
+
+namespace ServiceTest
+{
+    public class ConfigManagerTest
+    {
+        [Fact]
+        public void ConfigManager有Count屬性可以使用操作array方式取得configs的內容()
+        {
+            ConfigManager configManager = new ConfigManager();
+            configManager.ProcessConfig();
+
+            Assert.IsType<Config>(configManager[0]);
+            Assert.Equal(configManager.Count, 1);
+        }
+    }
+}
