cb7bee994e07fd26ddf50d16af31f0038911b8ab
guitarbien <guitarbien@gmail.com>
2017-10-29 21:52:06 +0800

    使用端統一由 MyBackupService 操作

---
+++ Service/MyBackupService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Service
+{
+    public class MyBackupService
+    {
+        private readonly ConfigManager configManager;
+
+        private readonly ScheduleManager scheduleManager;
+
+        public MyBackupService(ConfigManager configManager, ScheduleManager scheduleManager)
+        {
+            this.configManager = configManager ?? throw new ArgumentNullException(nameof(configManager));
+            this.scheduleManager = scheduleManager ?? throw new ArgumentNullException(nameof(scheduleManager));
+        }
+
+        public void ProcessConfig()
+        {
+            configManager.ProcessConfig();
+            scheduleManager.ProcessConfig();
+        }
+
+        public void ConfigService()
+        {
+
+        }
+    }
+}
---
+++ ServiceTest/MyBackupServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Design;
+using Xunit;
+using Service;
+
+namespace ServiceTest
+{
+    public class MyBackupServiceTest
+    {
+        [Fact]
+        public void 模擬使用端操作()
+        {
+            ConfigManager configManager = new ConfigManager();
+            ScheduleManager scheduleManager = new ScheduleManager();
+
+            MyBackupService myBackupService = new MyBackupService(configManager, scheduleManager);
+            myBackupService.ProcessConfig();
+            // myBackupService.DoBackup();
+        }
+    }
+}
