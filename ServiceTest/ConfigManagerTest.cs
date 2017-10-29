9695f15a79e3a547bb993bcbd9c2ac6006d01228
guitarbien <guitarbien@gmail.com>
2017-10-29 11:06:33 +0800

    測試加入 assert config values

--- ServiceTest/ConfigManagerTest.cs
+++ ServiceTest/ConfigManagerTest.cs
@@ -13,6 +13,16 @@ namespace ServiceTest
 
             Assert.IsType<Config>(configManager[0]);
             Assert.Equal(configManager.Count, 3);
+
+            Assert.Equal("cs", configManager[0].Ext);
+            Assert.Equal("c:\\\\Projects", configManager[0].Location);
+            Assert.True(configManager[0].SubDirectory);
+            Assert.Equal("file", configManager[0].Unit);
+            Assert.False(configManager[0].Remove);
+            Assert.Equal("zip", configManager[0].Handler);
+            Assert.Equal("directory", configManager[0].Destination);
+            Assert.Equal("c:\\\\MyArchieves", configManager[0].Dir);
+            Assert.Equal("", configManager[0].ConnectionString);
         }
     }
 }
