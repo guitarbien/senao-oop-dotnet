752f219b7fb863d57405b3ca135669407fc4b003
guitarbien <guitarbien@gmail.com>
2017-11-05 22:02:53 +0800

    EncodeHandler

---
+++ Service/Handler/EncodeHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Service.Handler
+{
+    public class EncodeHandler : AbstractHandler
+    {
+        public override byte[] Perform(Candidate candidate, byte[] target)
+        {
+            byte[] result = target;
+
+            base.Perform(candidate, target);
+
+            byte[] newResult = new byte[result.Length];
+
+            if (target != null)
+            {
+                for (int i = 0; i < result.Length; i++)
+                {
+                    newResult[i] = somethingEncode(result[i]);
+                }
+            }
+
+            return newResult;
+        }
+
+        private byte somethingEncode(byte input)
+        {
+            return input;
+        }
+    }
+}
