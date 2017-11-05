c33ad1e184932a5487d3df70c30bf2c98efa9cb4
guitarbien <guitarbien@gmail.com>
2017-11-05 14:54:22 +0800

    先決定 MyBackupService.DoBackup() 要如何使用

--- Service/MyBackupService.cs
+++ Service/MyBackupService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Service.Handler;
 
 namespace Service
 {
@@ -18,6 +19,52 @@ namespace Service
             {
                 manager.ProcessConfig();
             }
+        }
+
+        public void DoBackup()
+        {
+            List<Candidate> candidates = FindFiles();
+
+            foreach (Candidate candidate in candidates)
+            {
+                BroadcastToHandlers(candidate);
+            }
+        }
+
+        private List<Candidate> FindFiles()
+        {
+            List<Candidate> fakeCandidates = new List<Candidate>();
+
+            fakeCandidates.Add(new Candidate());
+
+            return fakeCandidates;
+        }
+
+        private void BroadcastToHandlers(Candidate candidate)
+        {
+            List<IHandler> handlers = FindHandlers(candidate);
+
+            byte[] target = null;
+            foreach (IHandler handler in handlers)
+            {
+                target = handler.perform(candidate, target);
+            }
+        }
+
+        private List<IHandler> FindHandlers(Candidate candidate)
+        {
+            List<IHandler> handlers = new List<IHandler>();
+
+            handlers.Add(HandlerFactory.create("file"));
+
+            foreach (string handler in candidate.config.handlers)
+            {
+                handlers.Add(HandlerFactory.create(handler));
+            }
+
+            handlers.Add(HandlerFactory.create(candidate.config.Destination));
+
+            return handlers;
         }
     }
 }
