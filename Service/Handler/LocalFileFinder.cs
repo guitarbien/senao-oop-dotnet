745e6f09b4d19c3deca4c0437aebd1bdaa2a2553
guitarbien <guitarbien@gmail.com>
2017-11-11 16:23:29 +0800

    實作 LocalFileFinder, constructor 取回 config.Location 下的所有符合的檔案

---
+++ Service/Handler/LocalFileFinder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Service.Handler
+{
+    public class LocalFileFinder : AbstractFileFinder
+    {
+        public LocalFileFinder(Config config) : base(config)
+        {
+            if (config.SubDirectory)
+            {
+                Files = GetSubDirectoryFiles(config);
+            }
+            else
+            {
+                Files = Directory.GetFiles(config.Location, "*." + config.Ext);
+            }
+        }
+
+        private string[] GetSubDirectoryFiles(Config config)
+        {
+            List<string> allFiles = new List<string>();
+
+            // 取出該層的所有檔案
+            allFiles.AddRange(Directory.GetFiles(config.Location, "*." + config.Ext));
+
+            // 加入該層的所有目錄下的檔案
+            foreach (string folder in Directory.GetDirectories(config.Location))
+            {
+                allFiles.AddRange(Directory.GetFiles(folder, "*." + config.Ext));
+            }
+
+            return allFiles.ToArray();
+        }
+
+        protected override Candidate CreateCandidate(string filename)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
