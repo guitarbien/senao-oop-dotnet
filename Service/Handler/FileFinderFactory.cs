756ea430ded6ad7d09daaff599acd134030b25a8
guitarbien <guitarbien@gmail.com>
2017-11-11 16:57:14 +0800

    DoBackup() 讀取 config.json 後透過 FileFinderFactory 建立 LocalFileFainder，並於 foreach 時對每個檔案產生 Candidate

---
+++ Service/Handler/FileFinderFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Service.Handler
+{
+    public class FileFinderFactory
+    {
+        public static IFileFinder Create(string finder, Config config)
+        {
+            if (finder == "file")
+            {
+                return new LocalFileFinder(config);
+            }
+
+            return null;
+        }
+    }
+}
