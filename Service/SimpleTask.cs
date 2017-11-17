5c145a871c398cb3550d0e302af7e35fe8eb0de9
guitarbien <guitarbien@gmail.com>
2017-11-18 00:43:37 +0800

    實作 SimpleTask 和 ScheduledTask

---
+++ Service/SimpleTask.cs
@@ -0,0 +1,17 @@
+﻿using MyBackupCandidate;
+
+namespace Service
+{
+    public class SimpleTask: AbstractTask
+    {
+        public override void Execute(Config config, Schedule schedule)
+        {
+            base.Execute(config, schedule);
+
+            foreach (Candidate candidate in FileFinder)
+            {
+                BroadcastToHandlers(candidate);
+            }
+        }
+    }
+}
