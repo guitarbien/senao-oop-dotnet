141367948038b3f03b7505f01f89a7e3e46818d0
guitarbien <guitarbien@gmail.com>
2017-11-26 19:40:49 +0800

    若有共用的部分放在 AbstractDbHandler

---
+++ Service/Handler/AbstractDbHandler.cs
@@ -0,0 +1,12 @@
+﻿using MyBackupCandidate;
+
+namespace Service.Handler
+{
+    public class AbstractDbHandler : IDbHandler
+    {
+        public virtual byte[] Perform(Candidate candidate, byte[] target)
+        {
+            return target;
+        }
+    }
+}
