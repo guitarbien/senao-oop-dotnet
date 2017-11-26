b263be08b82f3c4bc67c5b6c4c7c62181b945624
guitarbien <guitarbien@gmail.com>
2017-11-26 19:41:12 +0800

    宣告相關 model 和 db context

---
+++ Service/Models/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Service.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Backup> Backups { get; set; }
+        public DbSet<Log> Logs { get; set; }
+    }
+}
