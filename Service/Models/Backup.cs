623cab7753653245580af99d276e29ce0618ae8f
guitarbien <guitarbien@gmail.com>
2017-11-26 19:42:48 +0800

    Homework 06

---
+++ Service/Models/Backup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Service.Models
+{
+    public class Backup
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime FileDateTime { get; set; }
+        public long Size { get; set; }
+        public byte[] Target { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ Service/Models/Log.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Service.Models
+{
+    public class Log
+    {
+        public long Id { get; set; }
+        public DateTime fileDateTime { get; set; }
+        public bool createTime { get; set; }
+    }
+}
