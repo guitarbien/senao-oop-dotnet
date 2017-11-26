b88abbc162ce9e19dd9509085be88bb0bbab8cc1
guitarbien <guitarbien@gmail.com>
2017-11-26 19:41:22 +0800

    實作 db handler

---
+++ Service/Handler/DbBackupHandler.cs
@@ -0,0 +1,28 @@
+﻿using MyBackupCandidate;
+using Service.Models;
+
+namespace Service.Handler
+{
+    public class DbBackupHandler : AbstractDbHandler
+    {
+        public override byte[] Perform(Candidate candidate, byte[] target)
+        {
+            byte[] result = base.Perform(candidate, target);
+
+            using (var context = new MyDbContext())
+            {
+                var backup = new Backup
+                {
+                    Name = candidate.Name,
+                    FileDateTime = candidate.FileDateTime,
+                    Size = candidate.Size,
+                    Target = target
+                };
+
+                context.Backups.Add(backup);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Service/Handler/DbLogHandler.cs
@@ -0,0 +1,25 @@
+﻿using MyBackupCandidate;
+using Service.Models;
+
+namespace Service.Handler
+{
+    public class DbLogHandler : AbstractDbHandler
+    {
+        public override byte[] Perform(Candidate candidate, byte[] target)
+        {
+            byte[] result = base.Perform(candidate, target);
+
+            using (var context = new MyDbContext())
+            {
+                var log = new Log
+                {
+                    fileDateTime = candidate.FileDateTime
+                };
+
+                context.Logs.Add(log);
+            }
+
+            return result;
+        }
+    }
+}
