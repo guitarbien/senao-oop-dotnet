7d875099d7885896bd473086015aa4d7de709c28
guitarbien <guitarbien@gmail.com>
2017-11-26 19:39:12 +0800

    DbAdapter extends AbstractHandler to save() and log()

---
+++ Service/Handler/DbAdapter.cs
@@ -0,0 +1,30 @@
+﻿using MyBackupCandidate;
+
+namespace Service.Handler
+{
+    public class DbAdapter : AbstractHandler
+    {
+        private readonly IDbHandler _backupHandler = new DbBackupHandler();
+        private readonly IDbHandler _logHandler = new DbLogHandler();
+
+        public override byte[] Perform(Candidate candidate, byte[] target)
+        {
+            base.Perform(candidate, target);
+
+            SaveBackupToDb(candidate, target);
+            SaveLogToDb(candidate, target);
+
+            return target;
+        }
+
+        private void SaveLogToDb(Candidate candidate, byte[] target)
+        {
+            _backupHandler.Perform(candidate, target);
+        }
+
+        private void SaveBackupToDb(Candidate candidate, byte[] target)
+        {
+            _logHandler.Perform(candidate, target);
+        }
+    }
+}
